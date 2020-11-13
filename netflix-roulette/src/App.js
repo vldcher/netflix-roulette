9b16c657f698c22895b7ea462603f28cb0aba669
Vladyslav_Chernenko <vladyslav_chernenko@epam.com>
2020-11-13 11:41:26 +0200

    added ssr

--- netflix-roulette/src/App.js
+++ netflix-roulette/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.scss';
+import { Provider } from "react-redux";
+import { StaticRouter } from 'react-router';
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { addMovie } from "./store/actions/actionCreators";
+
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Main } from './components/Main';
@@ -7,29 +12,73 @@ import { NotFoundPage } from './components/NotFoundPage';
 import { ErrorBoundry } from './components/ErrorBoundry';
 import { PopupProvider } from './state/popupsContext';
 import { MoviesProvider } from './state/moviesContext';
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import './shared/buttons.scss';
 
 
-function App() {
+// const App = () => (
+//   <ErrorBoundry>
+//     <PopupProvider>
+//       <main className="App">
+//         <Router>
+//           <MoviesProvider>
+//             <Header />
+//             <Switch>
+//               <Route path="/" exact render={() => <Redirect to="/search" />} />
+//               <Route
+//                 path={["/search", "/search?=:searchTerm", "/film/:id"]}
+//                 component={Main}
+//               />
+//               <Route path="*" component={NotFoundPage} />
+//             </Switch>
+//           </MoviesProvider>
+//         </Router>
+//         <Footer />
+//       </main>
+//     </PopupProvider>
+//   </ErrorBoundry>
+// );
+
+const AppRoutes = ({ store }) => (
+  <Switch>
+    {/* <Route path="/" component={Home} exact /> */}
+    <Route path="/" exact render={() => <Redirect to="/search" />} />
+    <Route path={["/search", "/search?=:searchTerm", "/film/:id"]} render={() => {
+      if (store) {
+        store.dispatch(addMovie('This should come renderer from server (on /Page direct hit)'));
+      }
+
+      return <Main />;
+    }} exact />
+    <Route path="*" render={NotFoundPage} />
+  </Switch>
+)
+
+function App(props) {
   return (
     <ErrorBoundry>
       <PopupProvider>
         <main className="App">
-          <Router>
-            <MoviesProvider>
-              <Header />
-              <Switch>
-                <Route path="/" exact render={() => <Redirect to="/search" />} />
-                <Route
-                  path={["/search", "/search?=:searchTerm", "/film/:id"]}
-                  component={Main}
-                />
-                <Route path="*" component={NotFoundPage} />
-              </Switch>
-            </MoviesProvider>
-          </Router>
+          <Provider store={props.store}>
+            {
+              props.location
+                ? (
+                  <StaticRouter location={props.location} context={{}}>
+                    <MoviesProvider>
+                      <Header />
+                      <AppRoutes store={props.store} />
+                    </MoviesProvider>
+                  </StaticRouter>
+                ) : (
+                  <Router>
+                    <MoviesProvider>
+                      <Header />
+                      <AppRoutes />
+                    </MoviesProvider>
+                  </Router>
+                )
+            }
+          </Provider>
           <Footer />
         </main>
       </PopupProvider>
